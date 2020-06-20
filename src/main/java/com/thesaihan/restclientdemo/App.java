fadbc795e7cfb32c9dec380efc83adc2447401ed
Sai Han <thesaihan@gmail.com>
2020-06-20 22:27:31 +0630

    Trello List CRUD ListDao

--- src/main/java/com/thesaihan/restclientdemo/App.java
+++ src/main/java/com/thesaihan/restclientdemo/App.java
@@ -1,6 +1,6 @@
 package com.thesaihan.restclientdemo;
 
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,22 +8,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    private static OkHttpClient client = new OkHttpClient();
     public static void main( String[] args )
     {
+        // processCreate();
+        processRead();
+        // processUpdate();
+        // processDelete();
+    }
+
+	private static void processCreate() {
+        Map<String, Object> myNewList = new HashMap<String,Object>();
+        myNewList.put("title", "Another DK Card");
+        myNewList.put("position", 8);
+        myNewList.put("status", 1);
+        ListDao.create("/list", myNewList);
+    }
+
+    private static void processRead() {
         System.out.println("Trello List");
         System.out.println("-----------");
-        String listStr = getJsonStringFrom("https://trello-clone-ppm.herokuapp.com/list");
+        String listStr = ListDao.read("/list");
         try {
             List<Map<String, Object>> list = new ObjectMapper().readValue(listStr, List.class);
             for (Map<String,Object> map : list) {
@@ -40,17 +50,16 @@ public class App
 		}
     }
 
-    public static String getJsonStringFrom(String url) {
-        Request request = new Request.Builder()
-            .url(url)
-            .build();
+    private static void processUpdate() {
+        Map<String, Object> listToBeUpdated = new HashMap<String,Object>();
+        listToBeUpdated.put("title", "List created by DK team");
+        listToBeUpdated.put("position", 7);
+        listToBeUpdated.put("status", 1);
+        ListDao.update("/list", 47, listToBeUpdated);
+	}
 
-        try {
-            Response response = client.newCall(request).execute();
-            return response.body().string();
-        } catch(IOException e) {
-            e.printStackTrace();
-            return "";
-        }
+    private static void processDelete() {
+        ListDao.delete("/list", 48);
     }
+
 }
---
+++ src/main/java/com/thesaihan/restclientdemo/ListDao.java
@@ -0,0 +1,77 @@
+package com.thesaihan.restclientdemo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class ListDao {
+
+  public static final String END_POINT = "https://trello-clone-ppm.herokuapp.com";
+  public static final OkHttpClient client = new OkHttpClient();
+  public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+  public static String read(String url) {
+    Request request = new Request.Builder()
+            .url(END_POINT+url)
+            .build();
+    try {
+      Response response = client.newCall(request).execute();
+      return response.body().string();
+    } catch(IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public static String create(String url, Map<String, Object> list) {
+    try {
+      String listJsonString = new ObjectMapper().writeValueAsString(list);
+      RequestBody body = RequestBody.create(listJsonString, JSON);
+      Request request = new Request.Builder()
+            .url(END_POINT + url)
+            .post(body)
+            .build();
+      Response response = client.newCall(request).execute();
+      return response.body().string();
+    } catch(IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public static void delete(String url, int listId) {
+    try {
+      Request request = new Request.Builder()
+      .url(END_POINT  + url + "/" + listId)
+      .delete()
+      .build();
+      client.newCall(request).execute();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public static String update(String url, int id, Map<String, Object> list) {
+    try {
+      String listJsonStr = new ObjectMapper().writeValueAsString(list);
+      RequestBody body = RequestBody.create(listJsonStr, JSON);
+      Request request = new Request.Builder()
+        .url(END_POINT + url + "/" + id)
+        .put(body)
+        .build();
+      Response response = client.newCall(request).execute();
+      return response.body().string();
+    } catch (Exception e) {
+      return "";
+    }
+  }
+}
