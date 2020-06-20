e467c43e4257db48db4a8b07a2ac6b2e5b376d54
Sai Han <thesaihan@gmail.com>
2020-06-20 22:58:32 +0630

    Remove unused import in ListDao

--- src/main/java/com/thesaihan/restclientdemo/ListDao.java
+++ src/main/java/com/thesaihan/restclientdemo/ListDao.java
@@ -1,10 +1,8 @@
 package com.thesaihan.restclientdemo;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import okhttp3.MediaType;
